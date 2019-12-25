6db495affd3726c239ec43db3fd040fc0c8184bc
Bart Massey <bart.massey@gmail.com>
2019-12-25 01:01:28 -0800

    commented and documented

---
+++ README.md
@@ -0,0 +1,13 @@
+# Christmas Maze for Ben
+Bart Massey
+
+My son's Christmas Card this year needed a maze in
+it. Here's some Python code in a fairly rudimentary state
+that generates a circular SVG maze.
+
+Merry Christmas!
+
+---
+
+This program is licensed under the "MIT License". Please see
+the file `LICENSE` in this distribution for license terms.
--- maze.py
+++ maze.py
@@ -1,17 +1,26 @@
+# Generate a circular maze into `maze.svg`.
+
 import math
 import cairo
 import random
 
+# Fix the maze so that we can reproduce the result.
 random.seed(0)
 
+# Width of a square in SVG units (pixels).
 cellwidth = 10
+
+# Size of the image, including a small margin.
 dim = 22 * cellwidth
 
 def coord(x, y):
+    """Convert a maze cell coordinate to an SVG coordinate."""
     offset = cellwidth // 2
     return (x * cellwidth + offset, y * cellwidth + offset)
 
 class Cell(object):
+    """A single cell in the maze."""
+
     def __init__(self, x, y):
         self.loc = (x, y)
         self.neighbors = set()
@@ -20,16 +29,23 @@ class Cell(object):
         self.parent = None
 
     def add_neighbor(self, n):
+        """Remember a neighbor cell coordinate."""
         self.neighbors.add(n)
         self.walls.add(n)
 
     def add_border(self, n):
+        """Remember a border cell coordinate."""
         self.border.add(n)
 
     def coords(self):
+        """Where are we?"""
         return self.loc
 
     def render(self, ctx):
+        """Render this cell."""
+
+        # Offsets for line drawing, and corresponding
+        # offsets to neighbor.
         path = (
             ((+1, 0), (0, -1)),
             ((0, +1), (1, 0)),
@@ -37,6 +53,8 @@ class Cell(object):
             ((0, -1), (-1, 0)),
         )
 
+        # Walk around the edge of the cell drawing walls
+        # where appropriate.
         origin = self.loc
         x, y = origin
         prev = origin
@@ -53,18 +71,23 @@ class Cell(object):
             prev = cur
             
     def unvisit(self):
+        """Mark this cell as unvisited."""
         self.parent = None
 
     def visit(self, parent):
+        """Mark this cell as visited with the given parent."""
         self.parent = parent
     
     def visited(self):
+        """Has this cell been visited?"""
         return self.parent != None
     
     def remove_wall(self, where):
+        """Remove a wall from this cell."""
         self.walls.remove(where)
 
     def __str__(self):
+        """Render this cell for debugging."""
         return "[cell {}: n:{} w:{} b:{} p:{}]".format(
             self.loc,
             self.neighbors,
@@ -74,10 +97,12 @@ class Cell(object):
         )
 
 class Maze(object):
+    """A collection of cells with auxiliary information."""
     def __init__(self, dim):
         self.dim = dim
         self.border = set()
 
+        # Mark all the cells that are part of the maze.
         radius = dim // 2
         self.cells = dict()
         for row in range(-radius, radius + 1):
@@ -86,6 +111,7 @@ class Maze(object):
                 r, c = row + radius, col + radius
                 self.cells[(r, c)] = Cell(r, c)
 
+        # Set up all the neighbors and borders.
         for c in self.cells:
             x, y = c
             neighbors = (
@@ -103,36 +129,61 @@ class Maze(object):
                     self.cells[c].add_border(nxy)
     
     def render(self, ctx):
+        """Render each cell in the maze."""
+        # XXX This will result in each wall being drawn
+        # twice. Better would be to build up a set of walls
+        # and render them all in one pass.
         for _, c in self.cells.items():
             c.render(ctx)
 
     def unvisit(self):
+        """Unvisit all cells in the maze."""
         for _, c in self.cells.items():
             c.unvisit()
 
     def drill(self):
+        """Path the maze."""
+        # XXX This code should remove the entrance (border)
+        # wall. It should probably also clear a circle for
+        # Santa in the center before generation.
+
+        # XXX Hardcode the origin for now.
         c = self.cells[(10, 20)]
+        # XXX Gross hack to deal with the first cell having
+        # no parent.
         c.visit(True)
+
+        # DFS the maze, but shuffle the stack after each
+        # path step. So effectively the stack is just an
+        # open list.
         stack = []
         stack.append((10, 20))
         while len(stack) > 0:
             cc = stack.pop()
             c = self.cells[cc]
 
+            # Clear path to parent.
             pc = c.parent
             if pc != None and pc != True:
                 p = self.cells[pc]
                 c.remove_wall(pc)
                 p.remove_wall(cc)
 
-            neighbors = [n for n in c.neighbors if not self.cells[n].visited()]
+            # Add the neighbors to the open list.
+            neighbors = [n for n in c.neighbors
+                         if not self.cells[n].visited()]
             for n in neighbors:
                 self.cells[n].visit(cc)
             stack += neighbors
+            
+            # Shuffle for next round.
             random.shuffle(stack)
 
+# Make the maze.
 maze = Maze(20)
 maze.drill()
+
+# Render the maze.
 surface = cairo.SVGSurface("maze.svg", dim, dim)
 ctx = cairo.Context(surface)
 ctx.set_tolerance(0.01)
